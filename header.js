77fecd9d5564fed36f2ac98c23c0a0d28f8c1150
real-hallucination <85023583+real-hallucination@users.noreply.github.com>
2021-05-30 00:39:28 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,51 @@
+let header="<header>\
+<div class='container'>\
+    <nav class='navbar navbar-expand-lg navbar-light'>\
+        <a class='navbar-brand' href='index.html'>\
+            <img src='logo.png'>\
+        </a>\
+        <button class='navbar-toggler' type='button' data-toggle='collapse'\
+            data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false'\
+            aria-label='Toggle navigation'>\
+            <span class='navbar-toggler-icon'></span>\
+        </button>\
+        <div class='collapse navbar-collapse' id='navbarSupportedContent'>\
+            <ul class='navbar-nav mr-auto'>\
+                <li class='nav-item active'>\
+                    <a class='nav-link' href='index.html'>首頁</a>\
+                </li>\
+                <!--<li class='nav-item active'>\
+                    <a class='nav-link' href='#'>師長的話</a>\
+                </li>-->\
+                <li class='nav-item dropdown'>\
+                    <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button'\
+                        data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>\
+                        專題\
+                    </a>\
+                    <div class='dropdown-menu' aria-labelledby='navbarDropdown'>\
+                        <a class='dropdown-item' href='chm.html'>化學</a>\
+                        <a class='dropdown-item' href='bio.html'>生物</a>\
+                        <a class='dropdown-item' href='egn.html'>工程</a>\
+                        <a class='dropdown-item' href='IT.html'>資訊</a>\
+                        <a class='dropdown-item' href='math.html'>數學</a>\
+                        <a class='dropdown-item' href='phy.html'>物理</a>\
+                    </div>\
+                </li>\
+                <li class='nav-item dropdown'>\
+                    <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button'\
+                        data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>\
+                        影音相關\
+                    </a>\
+                    <div class='dropdown-menu' aria-labelledby='navbarDropdown'>\
+                        <a class='dropdown-item' href='https://www.youtube.com/watch?v=CYHws4geDNA' target='_blank'>預告片-恐怖巨獻</a>\
+                        <a class='dropdown-item' href='https://www.youtube.com/watch?v=O1s8OEzlBN0' target='_blank'>預告片-患蛋見真情</a>\
+                        <a class='dropdown-item' href='#'>患蛋</a>\
+                        <a class='dropdown-item' href='https://www.youtube.com/channel/UCNMCqiRPasl9T1VudKd7KMQ/featured' target='_blank'>線上成發精彩重播</a>\
+                    </div>\
+                </li>\
+            </ul>\
+        </div>\
+    </nav>\
+</div>\
+</header>";
+document.querySelector('.header').innerHTML=header;
---
+++ not-found.js
@@ -0,0 +1,193 @@
+var a="<h1 align='center'><big>報告還在虛幻之中<br>敬請期待</big></h1><br>";
+var b="<div class='header'></div>\
+<script src='header.js'></script>\
+<section id='main'>\
+    <div class='main-picture'>\
+        <div class='container'>\
+            <div class='row'>\
+                <div class='col-md-12'>\
+                    <h1>實 | <svg><text>Hallucination</text></svg></h1>\
+                    <p class='lead'>台中一中第十屆科學班成果發表會</p>\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+</section>\
+<section id='intro'>\
+    <div class='container'>\
+        <div class='row'>\
+            <div class='col-md-8 offset-md-2 text-center'>\
+                <h3>主題介紹</h3>\
+                <div class='row'>\
+                    <div class='col-md-3'>\
+                        <img src='logo.png'>\
+                        <h4 class='wordCarousel'>\
+                            <div>\
+                                <ul class='flip2'>\
+                                    <li>Reality</li>\
+                                    <li>Hallucination</li>\
+                                </ul>\
+                            </div>\
+                        </h4>\
+                    </div>\
+                    <div class='col-md-9 text-left'>\
+                        <p>這個主題其實是將最初的兩個想法合併的：幻夢/Hallucination、實。</p>\
+                        <p>一眨眼，高中三年的旅程就將要結束，就像夢一場，沒什麼真實感，但回頭審視，的確也經歷了許多。不知這三年是我們即將清醒的一場幻夢，抑或是我們一路上所追尋的夢幻？</p>\
+                        <p>實，很基本的，就是從第十屆科學班的「十」去發想，畢竟這是我們與其他科學班學生最直接的差別。「實」可以延伸出許多詞彙：成果發表是向人呈現高中三年所結出的果實，而這不只是指科學實驗，還有團體合作的方面。</p>\
+                        <p>我們將這兩種方向結合在一起，恰好形成了一股對立。真真假假、虛虛實實，我們想找到個定論，不論是科學方面、還是實際生活，但，找的到嗎？</p>\
+                        <p>Hallucination，通常是因為疾病或是藥物所造成幻覺。我們將以科學狂熱為病，以事實真理為癮，暈眩在這一場幻夢當中。</p>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+</section>\
+<!--<section id='time-table'>\
+    <div class='container'>\
+        <div class='row'>\
+            <div class='col-md-8 offset-md-2 text-center'>\
+                <h3>時間表</h3>\
+                <h4>交大場</h4>\
+                <div class='row'>\
+                    <div class='col-md-6 text-center'>\
+                        <h5>次軒廳</h5>\
+                        <table border='1' align='center'>\
+                            <tr><th width=120px>時間</th><th width=160px>活動</th></tr>\
+                            <tr><td>12:30~??:??</td><td>開幕式</td></tr>\
+                        </table>\
+                    </div>\
+                    <div class='col-md-6 text-center'>\
+                        <h5>物理教室</h5>\
+                        <table border='1' align='center'>\
+                            <tr><th width=120px>時間</th><th width=160px>活動</th></tr>\
+                            <tr><td>12:30~??:??</td><td>開幕式</td></tr>\
+                        </table>\
+                    </div>\
+                </div>\
+                <h4>一中場</h4>\
+                <table border='1' align='center'>\
+                    <tr><th width=120px>時間</th><th width=160px>活動</th></tr>\
+                    <tr><td>9:00~??:??</td><td>開幕式</td></tr>\
+                </table>\
+            </div>\
+        </div>\
+    </div>\
+</section>-->\
+<div class='footer'></div>\
+<script src='footer.js'></script>"
+var c="<div class='header'></div>\
+<script src='header.js'></script>\
+<section id='main'>\
+    <div class='main-picture'>\
+        <div class='container'>\
+            <div class='row'>\
+                <div class='col-md-12'>\
+                    <h1>虛 | <svg><text>Reality</text></svg></h1>\
+                    <p class='lead'>台中一中第十屆科學班成果發表會</p>\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+</section>\
+<section id='intro'>\
+    <div class='container'>\
+        <div class='row'>\
+            <div class='col-md-8 offset-md-2 text-center'>\
+                <h3>主題介紹</h3>\
+                <div class='row'>\
+                    <div class='col-md-3'>\
+                        <img src='logo.png'>\
+                        <h4 class='wordCarousel'>\
+                            <div>\
+                                <ul class='flip2'>\
+                                    <li>Reality</li>\
+                                    <li>Hallucination</li>\
+                                </ul>\
+                            </div>\
+                        </h4>\
+                    </div>\
+                    <div class='col-md-9 text-left'>\
+                        <p>這個主題其實是將最初的兩個想法合併的：幻夢/Hallucination、實。</p>\
+                        <p>一眨眼，高中三年的旅程就將要結束，就像夢一場，沒什麼真實感，但回頭審視，的確也經歷了許多。不知這三年是我們即將清醒的一場幻夢，抑或是我們一路上所追尋的夢幻？</p>\
+                        <p>實，很基本的，就是從第十屆科學班的「十」去發想，畢竟這是我們與其他科學班學生最直接的差別。「實」可以延伸出許多詞彙：成果發表是向人呈現高中三年所結出的果實，而這不只是指科學實驗，還有團體合作的方面。</p>\
+                        <p>我們將這兩種方向結合在一起，恰好形成了一股對立。真真假假、虛虛實實，我們想找到個定論，不論是科學方面、還是實際生活，但，找的到嗎？</p>\
+                        <p>Hallucination，通常是因為疾病或是藥物所造成幻覺。我們將以科學狂熱為病，以事實真理為癮，暈眩在這一場幻夢當中。</p>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+</section>\
+<!--<section id='time-table'>\
+    <div class='container'>\
+        <div class='row'>\
+            <div class='col-md-8 offset-md-2 text-center'>\
+                <h3>時間表</h3>\
+                <h4>交大場</h4>\
+                <div class='row'>\
+                    <div class='col-md-6 text-center'>\
+                        <h5>次軒廳</h5>\
+                        <table border='1' align='center'>\
+                            <tr><th width=120px>時間</th><th width=160px>活動</th></tr>\
+                            <tr><td>12:30~??:??</td><td>開幕式</td></tr>\
+                        </table>\
+                    </div>\
+                    <div class='col-md-6 text-center'>\
+                        <h5>物理教室</h5>\
+                        <table border='1' align='center'>\
+                            <tr><th width=120px>時間</th><th width=160px>活動</th></tr>\
+                            <tr><td>12:30~??:??</td><td>開幕式</td></tr>\
+                        </table>\
+                    </div>\
+                </div>\
+                <h4>一中場</h4>\
+                <table border='1' align='center'>\
+                    <tr><th width=120px>時間</th><th width=160px>活動</th></tr>\
+                    <tr><td>9:00~??:??</td><td>開幕式</td></tr>\
+                </table>\
+            </div>\
+        </div>\
+    </div>\
+</section>-->\
+<div class='footer'></div>\
+<script src='footer.js'></script>"
+var time=0;
+function index(){
+    document.querySelector('.not-found').innerHTML=b;
+    setTimeout("init()", 5000);
+}
+function xedni(){
+    document.querySelector('.not-found').innerHTML=c;
+    setTimeout("init()", 5000);
+}
+function real(){
+    a="<h1 align='center'><big>報告還在虛幻之中<br>敬請期待</big><br><br>你確定這就是真實世界嗎...？</h1><br>";
+    document.querySelector('.not-found').innerHTML=a;
+    setTimeout("index()", 2000);
+}
+function hallucination(){
+    a="<h1 align='center'><big>報告還在虛幻之中<br>敬請期待</big><br><br>這是夢嗎？為什麼那麼真實...</h1><br>";
+    document.querySelector('.not-found').innerHTML=a;
+    setTimeout("xedni()", 2000);
+}
+function init(){
+    ++time;
+    a="<h1 align='center'><big>報告還在虛幻之中<br>敬請期待</big></h1><br>";
+    if(time>3){
+        a+="<h1 align='center'>你知道為什麼逃不掉嗎？　　　<br>　　　因為，你從未真正離開過</h1>";
+        setTimeout("javascript:location.href='https://www.youtube.com/watch?v=CYHws4geDNA'", 2000);
+    }
+    else setTimeout("ask()", 2000);
+    document.querySelector('.not-found').innerHTML=a;
+}
+function ask(){
+    a+="<div align='center'><span>您想待在虛幻之中，抑或回到現實呢？<br></span>\
+    <button onclick='hallucination()'>待在虛幻</button>　　<button onclick='real()'>回到現實</button></div>";
+    document.querySelector('.not-found').innerHTML=a;
+    if(time==3)setTimeout("ask2()", 2000);
+}
+function ask2(){
+    a+="<br><div align='center'><span>真的沒有別的<button onclick='hallucination()'>選擇</button>了嗎？</span></div>"
+    document.querySelector('.not-found').innerHTML=a;
+}
+init();
